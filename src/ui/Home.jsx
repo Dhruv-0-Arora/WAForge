991a36f9d2e6fa7fe7dcad71ba6a7e01aa8d042e
Dhruv Arora <dhruv.arora1@autodesk.com>
2025-02-15 15:58:59 -0800

    Fixed scaling

--- src/ui/Home.jsx
+++ src/ui/Home.jsx
@@ -4,37 +4,41 @@ import globePlane from "../assets/flight.png"; // The center image
 
 export default function Home() {
     return (
-        <div>
-            {/* Space */}
-            <div className="h-[5vh]"></div>
+        <div className="flex">
+            <div className="inline-flex flex-col items-center justify-between min-h-screen w-auto mx-auto">
+                {/* Top spacing */}
+                <div className="h-[5vh]" />
 
-            {/* "ENVORA" with an image mask */}
-            <h1
-                className="flex justify-center align-center text-center text-[96px] font-bold select-none brightness-80"
-                style={{
-                    backgroundImage: `url(${imageMask})`,
-                    backgroundSize: "cover",
-                    backgroundPosition: "center",
-                    webkitTextFillColor: "transparent",
-                    webkitBackgroundClip: "text",
-                }}
-            >
-                ENVORA
-            </h1>
+                {/* "ENVORA" with an image mask */}
+                <h1
+                    className=" flex justify-center items-center text-center text-[64px] font-bold select-none brightness-80 md:text-[96px]"
+                    style={{
+                        backgroundImage: `url(${imageMask})`,
+                        backgroundSize: "cover",
+                        backgroundPosition: "center",
+                        WebkitTextFillColor: "transparent",
+                        WebkitBackgroundClip: "text",
+                    }}
+                >
+                    ENVORA
+                </h1>
 
-            {/* Center image */}
-            <div className="flex justify-center align-center h-auto">
-                <img
-                    src={globePlane}
-                    alt="Earth with airplane"
-                    className="max-w-[75vw] h-auto"
-                />
+                {/* Center image */}
+                <div className="flex justify-center items-center h-auto">
+                    <img
+                        src={globePlane}
+                        alt="Earth with airplane"
+                        className="max-w-[85vw] h-auto md:max-w-[700px]"
+                    />
+                </div>
+
+                {/* Tagline */}
+                <p className="text-center text-3xl md:text-4xl font-bold text-gray-500 px-5">
+                    Your Guide to Sustainable Living
+                </p>
+
+                <div className="h-[5vh]"></div>
             </div>
-
-            {/* Tagline */}
-            <p className="text-center text-3xl font-bold text-gray-500 font-bold">
-                Your Guide to Sustainable Living
-            </p>
         </div>
     );
 }
