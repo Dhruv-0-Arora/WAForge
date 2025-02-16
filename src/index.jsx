c004082b00b15b6bed0ca7309cf683a5807a06d3
Rishi Vora <87199706+rishijv@users.noreply.github.com>
2025-02-16 00:00:02 -0800

    working about page route

--- src/index.jsx
+++ src/index.jsx
@@ -5,13 +5,14 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import reportWebVitals from "./reportWebVitals";
 import Home from "./ui/Home";
-import About from "./ui/About";
 import PlaneTracker from "./ui/PlaneTracker";
+import About from "./ui/About"; // Ensure this is imported
 
+// Define routes
 const router = createBrowserRouter([
     { path: "/", element: <Home /> },
     { path: "/tracker", element: <PlaneTracker /> },
-
+    { path: "/about", element: <About /> }, // Ensure this exists
 ]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
@@ -21,7 +22,4 @@ root.render(
     </React.StrictMode>,
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
